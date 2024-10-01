2c95dc6707c435f183d7934ad493b010cdbabdfe
Saurav Sharma <saurav5122000@gmail.com>
2024-10-01 19:44:42 +0530

    Basics of python

---
+++ basics/intro.py
@@ -0,0 +1,5 @@
+second_num = 1
+second_num = 2
+
+sum = second_num + second_num
+print('sum of both is : ',sum)
---
+++ basics/string_multiply.py
@@ -0,0 +1,11 @@
+name = "hello "
+multi = name * 5
+
+print('output', multi)
+
+
+# hashtag used for commenting
+
+"""
+Multi line comment  : Python ignores them during execution.
+"""
