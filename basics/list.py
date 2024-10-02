8f8cb1161dc85fbbf45185ff58a79ed8eed07866
Saurav Sharma <saurav5122000@gmail.com>
2024-10-02 19:28:22 +0530

    python loops and conditions

---
+++ basics/list.py
@@ -0,0 +1,23 @@
+my_list = [1, 2, 34,56]
+# print(my_list[0]) 
+# print(my_list[3]) 
+
+len(my_list)
+
+my_list[1] = 20 
+my_list.append(6)  
+my_list.remove(56)  
+
+# print(my_list)
+
+
+# for item in my_list:
+    # print(item)
+    # print(item + 5)
+
+
+# n = 0
+# while n < 5:
+#     print(n)
+#     n = n + 1 
+
