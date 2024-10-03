20fa0468b4cb4fd6eb6451756ed6870a6da803e0
Saurav Sharma <saurav5122000@gmail.com>
2024-10-03 20:47:42 +0530

    python if-else and match cases

--- basics/list.py
+++ basics/list.py
@@ -21,3 +21,49 @@ my_list.remove(56)
 #     print(n)
 #     n = n + 1 
 
+
+# nested for loop 
+
+# for i in range(3):
+#     for j in range(2):
+#         print(f'i: {i}, j: {j}')
+
+
+# no do while in python
+
+
+# if else condition 
+
+
+# x1 = int(input("Please enter an integer: "))      // basic odd even 
+
+# if x1 % 2 == 0:
+#     print('even number')
+# else:
+#     print('odd number')
+
+
+# list range 
+# print(list(range(10,20)))
+
+
+# print(sum(range(0,9)))
+
+# number_list = [1,2,3,4,5,6,7]
+
+# for val in number_list:
+#     if val % 2 == 0:
+#         print(f"even nos are : {val}")
+#     else:
+#         print(f"odd nos : {val}")
+
+
+# def hello(2)
+# def hello(num):               // similar to switch case
+#     match num:
+#         case 1:
+#             return 1
+#         case 2:
+#             return 2
+#         case _:
+#             return 'nothing'
