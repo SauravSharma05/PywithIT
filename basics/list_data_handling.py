fa2cfda0d697feeaf87c17a1552117d035bea4e2
Saurav Sharma <saurav5122000@gmail.com>
2024-10-06 18:12:06 +0530

    python list handling like stack and queue

---
+++ basics/list_data_handling.py
@@ -0,0 +1,38 @@
+vehicles = ['car', 'bike', 'ship', 'boat']
+
+
+# clear all data in the list
+# vehicles.clear()
+# print('empty vehicles',vehicles)   
+# 
+
+
+# reverse all data in the list
+# vehicles.reverse()
+# print('reverse vehicles',vehicles)   
+# 
+
+# add data in end of the list
+# vehicles.append()
+# print('vehicles',vehicles)   
+# 
+
+
+# count the no of times of specific data appearing in the list
+# vehicles.count('boat')
+# print('no of times boat appears',vehicles)   
+# 
+
+
+# add data in end of the list
+# vehicles.sort()
+# print('sorting',vehicles)   
+
+
+# we can use append and pop to make a list work like a stack
+
+# we can use append and popleft to make a list work like a queue
+
+
+
+
