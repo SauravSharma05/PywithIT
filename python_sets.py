0ff554d219f1fec42dc297789cce080f91c6d8b8
Saurav Sharma <saurav5122000@gmail.com>
2024-10-07 19:15:52 +0530

    python sets

---
+++ python_sets.py
@@ -0,0 +1,27 @@
+# to remove specific items from the list
+# list = [1,2,3,4,5]
+# del(list[2]) // will remove 3 here
+# del(a[:]) // deletes whole list
+# del(a[1:2]) // deleting only specific part
+# delete list // deletes entire variable
+
+# TUPLES
+
+a1 = 1, 2, 'hi', 89
+# print(a1[2]) // works like list
+# a2 = 7,8,9
+
+# a1 = a1, a2
+# print(a1) // shows both
+
+# SETS
+# does not show duplicate data 
+# hello = {1,2,3,3,4,4,55,55,6,6,9}
+# hi = {1,2,3,4,6,6,6,8,9,9,9,10}
+# print(hello)
+# print(hi)
+# print(hello - hi)      // hello data only
+# print(hello | hi)      // all data without duplicate
+# print(hello ^ hi)      // different data of both
+# print(hello & hi)       // data of both 
+
