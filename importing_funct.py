9fcded3b89808f42349c71dcf806c47eecdbdd3d
Saurav Sharma <saurav5122000@gmail.com>
2024-10-08 20:14:02 +0530

    importing functions in python

---
+++ importing_funct.py
@@ -0,0 +1,36 @@
+# items= ['ball', 'airplane' , 'cat', 'dog',''yatch]
+
+# for vali in sorted(items):
+#       print(vali)                              // will show all sorted data 
+
+
+# for vali in sorted(set(items)):
+#       print(vali)                              // will show all sorted data without duplicate
+
+
+# # modules in python
+# create a file name as module1.py
+# write any function it it
+# def addno(a,b):
+#       return a+b
+
+# create another file mainfile.py
+# then
+# import module1
+# a = 15
+# b = 86
+# result1 = module1.addno(b,a)
+# print(result1)
+
+
+# importing styles :
+#from filename import fun1, fun2            // specific functions
+#from filename import *            // all function
+f#rom filename import fun1 as function_1      // with naming 
+
+
+# use command python filename // to run code of specific file
+
+# a = 10
+# b = 20
+# print({a=},(b=))            // used for debugging
