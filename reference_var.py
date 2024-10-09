f94dbcb164966eba30fe0f0f8cab5529efd80a0e
Saurav Sharma <saurav5122000@gmail.com>
2024-10-09 20:04:24 +0530

    reference variables python

---
+++ reference_var.py
@@ -0,0 +1,22 @@
+# a = 10
+# b = 'hi'
+# print(f'we can print both variables as {a} and {b}')
+
+# str and repr are two debugging tools for converting to any string 
+# text1 = 'hello, motto'
+# debug_text = str(text1)
+# print(debug_text)
+
+# text2 = 'hello, motto'
+# debug_text = repr(text2)
+# print(debug_text)
+
+
+# !a : ascii
+# !s : str()
+# !r : repr()
+
+# formatting text
+# print('this is {} and {}'.format('a', 'b'))
+# print('this is {0} and {1}'.format('a', 'b'))   // for position
+
