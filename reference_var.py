5557ed8b811217fcf3c294a4ea7362dbd58a3b4b
Saurav Sharma <saurav5122000@gmail.com>
2024-10-10 19:25:19 +0530

    function scope in python

--- reference_var.py
+++ reference_var.py
@@ -20,3 +20,38 @@
 # print('this is {} and {}'.format('a', 'b'))
 # print('this is {0} and {1}'.format('a', 'b'))   // for position
 
+# def local_class_ex:
+# def local_func:
+# text1 = 'local hello'
+
+# def non_local_func:
+# text1 = 'non local hello'
+
+# def global_func:
+# text1 = 'non local hello'
+
+# text1 = "normal hello"
+
+# local_func()
+# print('local function output : ', text1)
+
+
+# non_local_func()
+# nonlocal text1
+# print('non local function output : ', text1)
+
+
+# global_func()
+# global text1
+# print('global function output : ', text1)
+
+# local_class_ex()
+# print('global output : ', text1)
+
+
+# here the first function defines a local variable text1 within its own scope. This local text1 can only be accessed within local_func
+# second function it modifies the text1 variable that exists in the scope of local_class_ex
+# third function modifies the global variable text1, which is defined outside any function. By using the global keyword, it allows changes to this global variable.
+
+
+
