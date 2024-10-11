7677ca1a5c14a7ba579f7b143e17dc0300545fef
Saurav Sharma <saurav5122000@gmail.com>
2024-10-11 18:06:24 +0530

    class obj

---
+++ class_n_object.py
@@ -0,0 +1,13 @@
+# class car:
+#    car1 = 'mercedes'
+#    car2 = 'bmw'
+
+# name1 = car()
+# print(name1.car2, name1.car1)
+
+# name2 = car()
+# print(name2.car1)               // similarly we can make another object with same 
+
+
+
+
