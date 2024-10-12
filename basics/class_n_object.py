6a3fe2b015321f0f726266ad9911a912cae6d33e
Saurav Sharma <saurav5122000@gmail.com>
2024-10-12 20:13:07 +0530

    class obj python

---
+++ basics/class_n_object.py
@@ -0,0 +1,22 @@
+# class car:
+#    car1 = 'mercedes'
+#    car2 = 'bmw'
+
+# name1 = car()
+# print(name1.car2, name1.car1)
+
+# name2 = car()
+# print(name2.car1)               // similarly we can make another object with same 
+
+
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+#     def bark(self):
+#         print(f"{self.name} say hi!")
+
+# my_dog = Dog("hello", "german shephard")
+# my_dog.bark()
+
