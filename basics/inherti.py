6870276661a208a82d1abe0f0d104db113f3d53c
Saurav Sharma <saurav5122000@gmail.com>
2024-10-24 18:52:00 +0530

    heirar, multilevel inherit python

--- basics/inherti.py
+++ basics/inherti.py
@@ -24,8 +24,47 @@ class Child(Father, Mother):
     def hobbies(self):
         print("Child's hobbies")
 
+# multilevel
 child_instance = Child()
 child_instance.skills()   
 child_instance.talents()  
 child_instance.hobbies()   
 
+class Grandparent:
+    def heritage(self):
+        print("Grandparent's heritage")
+
+class Parent(Grandparent):
+    def profession(self):
+        print("Parent's profession")
+
+class Child(Parent):
+    def future(self):
+        print("Child's future plans")
+
+child_instance = Child()
+child_instance.heritage()   
+child_instance.profession()  
+child_instance.future()      
+
+
+# heirarchical
+class Animal:
+    def sound(self):
+        print("Animal sound")
+
+class Dog(Animal):
+    def bark(self):
+        print("Dog barks")
+
+class Cat(Animal):
+    def meow(self):
+        print("Cat meows")
+
+dog_instance = Dog()
+cat_instance = Cat()
+dog_instance.sound()  
+dog_instance.bark()   
+
+cat_instance.sound()  
+cat_instance.meow()   
