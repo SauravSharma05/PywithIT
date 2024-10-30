94e2a11c8e1a8b6b68da89591de5dc69d6e4d37a
Saurav Sharma <saurav5122000@gmail.com>
2024-10-30 07:16:18 +0530

    find method

--- basics/string.py
+++ basics/string.py
@@ -14,3 +14,25 @@ main_string = "GfG is a good website"
 substrings = ["GfG", "site", "CS", "Geeks", "Tutorial"]
 matching_substrings = list(filter(lambda x: x in main_string, substrings))
 print("Matching substrings:", matching_substrings)
+
+# find method
+main_string = "GfG is a good website"
+substrings = ["GfG", "site", "CS", "Geeks", "Tutorial"]
+matching_substrings = []
+for sub in substrings:
+    if main_string.find(sub) != -1:
+        matching_substrings.append(sub)
+
+print("Matching substrings:", matching_substrings) 
+
+
+
+# re expression : regular expression
+
+import re
+
+main_string = "GfG is a good website"
+substrings = ["GfG", "site", "CS", "Geeks", "Tutorial"]
+matching_substrings = [sub for sub in substrings if re.search(re.escape(sub), main_string)]
+
+print("Matching substrings:", matching_substrings)
