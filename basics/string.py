fcf5ce05bad61ed3f19e2fa4059e5c2e1e918d2f
Saurav Sharma <saurav5122000@gmail.com>
2024-11-03 13:37:34 +0530

    string functions

--- basics/string.py
+++ basics/string.py
@@ -36,3 +36,14 @@ substrings = ["GfG", "site", "CS", "Geeks", "Tutorial"]
 matching_substrings = [sub for sub in substrings if re.search(re.escape(sub), main_string)]
 
 print("Matching substrings:", matching_substrings)
+
+# string functions 
+text = "Hello, World"
+
+first_character = text[0]  # 'H'
+last_character = text[-1]   # '!'
+length_of_text = len(text)  # 13
+
+substring = text[7:12]  # 'World'
+
+print(first_character, last_character, length_of_text, substring)
