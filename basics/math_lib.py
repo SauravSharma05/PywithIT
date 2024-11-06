6c025bb1bb3a364656e2cba07fce2d29e475f705
Saurav Sharma <saurav5122000@gmail.com>
2024-11-06 19:49:28 +0530

    math lib func

---
+++ basics/math_lib.py
@@ -0,0 +1,28 @@
+import math
+
+x = math.sqrt(16)
+print(x)  
+
+result = math.pow(2, 3)
+print(result)  
+
+abs_value = abs(-10)
+print(abs_value)  
+
+ceil_value = math.ceil(4.3)
+floor_value = math.floor(4.7)
+print(ceil_value)  
+print(floor_value)  
+
+
+fact = math.factorial(5)
+print(fact)  
+
+
+gcd_value = math.gcd(48, 18)
+print(gcd_value)  
+
+# euler's number
+exp_value = math.exp(1)
+print(exp_value)
+
