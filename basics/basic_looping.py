6f0198f8bc66cdd2621532eaf0f4d7c125477cae
Saurav Sharma <saurav5122000@gmail.com>
2024-11-10 11:53:27 +0530

    looping in patterns

--- basics/basic_looping.py
+++ basics/basic_looping.py
@@ -13,3 +13,24 @@ def triangle_star_pattern(rows):
         print()
 
 triangle_star_pattern(5)
+
+# diamond 
+def diamond_pattern(rows):
+    for i in range(rows):
+        print(" " * (rows - i - 1), end="")
+        print("* " * (i + 1))
+    
+    for i in range(rows - 2, -1, -1):
+        print(" " * (rows - i - 1), end="")
+        print("* " * (i + 1))
+
+diamond_pattern(5)
+
+
+def invertednumbers(rows):
+    for i in range(rows, 0, -1):
+        for j in range(i):
+            print(i, end=" ")
+        print()
+
+invertednumbers(5)
