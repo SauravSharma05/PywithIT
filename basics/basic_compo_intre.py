171cb7981bf2b7fa9a46fceaad2103f910579638
Saurav Sharma <saurav5122000@gmail.com>
2024-11-11 19:56:31 +0530

    compunt int. calc

---
+++ basics/basic_compo_intre.py
@@ -0,0 +1,21 @@
+#basic compund interest calc
+
+def calculate_compound_interest(principal, rate, times_compounded, years):
+    rate_decimal = rate / 100
+    total_amount = principal * (1 + rate_decimal / times_compounded) ** (times_compounded * years)
+    return total_amount
+
+def main():
+    print("Welcome to the Compound Interest Calculator!")
+
+    principal = float(input("Enter the principal amount : "))
+    rate = float(input("Enter annual interest rate : "))
+    times_compounded = int(input("Enter number of times that interest is compounded: "))
+    years = float(input("Enter years : "))
+
+    total_amount = calculate_compound_interest(principal, rate, times_compounded, years)
+
+    print(f"\nTotal amount after {years} years: ${total_amount:.2f}")
+
+if __name__ == "__main__":
+    main()
