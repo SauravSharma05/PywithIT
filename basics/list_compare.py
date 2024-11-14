682034a7bdce92a7c8c8bfcbcf3a1e65091768f0
Saurav Sharma <saurav5122000@gmail.com>
2024-11-14 20:19:38 +0530

    map, reduce

--- basics/list_compare.py
+++ basics/list_compare.py
@@ -19,3 +19,20 @@ list2 = [1, 5, 3]
 
 # Sort and compare
 print(sorted(list1) == sorted(list2))  
+
+
+
+#map and reduce
+
+from functools import reduce
+
+list1 = [10, 20, 30, 40, 50]
+list2 = [10, 20, 30, 40, 50]
+list3 = [10, 20, 30, 40, 60]
+
+def compare_lists(l1, l2):
+    comparison_results = map(lambda x, y: x == y, l1, l2)
+    return reduce(lambda x, y: x and y, comparison_results)
+
+print("List1 and List2 are equal:", compare_lists(list1, list2))  
+print("List1 and List3 are equal:", compare_lists(list1, list3))  
